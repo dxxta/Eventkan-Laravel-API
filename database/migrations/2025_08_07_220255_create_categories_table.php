ed78e51e0cbba156022474c79382b2958e8e9860
dxxta <d.nta.workspace@gmail.com>
2025-08-09 01:01:16 +0700

    feat: update field

--- database/migrations/2025_08_07_220255_create_categories_table.php
+++ database/migrations/2025_08_07_220255_create_categories_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->boolean('is_removed')->default(false);
+            $table->datetime('deleted_at')->nullable();
             $table->timestamps();
         });
     }
