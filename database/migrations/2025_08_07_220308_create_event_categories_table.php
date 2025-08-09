2d152e87445600227755631d608a49da6dd6ed21
dxxta <d.nta.workspace@gmail.com>
2025-08-10 06:49:41 +0700

    feat: new migration for event_categories, remove id for pivot table

--- database/migrations/2025_08_07_220308_create_event_categories_table.php
+++ database/migrations/2025_08_07_220308_create_event_categories_table.php
@@ -12,7 +12,6 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('event_categories', function (Blueprint $table) {
-            $table->uuid("id")->primary();
             $table->uuid('event_id')->nullable()->index();
             $table->foreign('event_id')->references('id')->on('events')->onDelete('cascade');
             $table->uuid('category_id')->nullable()->index();
